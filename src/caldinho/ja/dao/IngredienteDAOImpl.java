093235dda67fe30167fed74920ec5fbed004e4c6
lgdamy <lgdamy@gmail.com>
2017-04-14 14:52:19 -0300

    ingredienteDAO

---
+++ src/caldinho/ja/dao/IngredienteDAOImpl.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caldinho.ja.dao;
+
+import caldinho.ja.escopo.Ingrediente;
+import java.util.List;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author lgalmeida
+ */
+public class IngredienteDAOImpl implements IngredienteDAO {
+
+    private EntityManager em;
+
+    public IngredienteDAOImpl(EntityManager em) {
+        this.em = em;
+    }
+    
+    @Override
+    public void novoIngrediente(Ingrediente ingrediente){
+        em.persist(ingrediente);
+    }
+    @Override
+    public Ingrediente getIngrediente(int id){
+        return fetchIngredientes().get(id);
+    }
+    @Override
+    public void deleteIngrediente(int id){
+        em.remove(getIngrediente(id));
+    }
+    @Override
+    public void atualizaIngrediente(Ingrediente ingrediente){
+        em.merge(ingrediente);
+    }
+    @Override
+    public List<Ingrediente> fetchIngredientes(){
+        return em.createQuery("SELECT ingrediente FROM Ingrediente ingrediente", Ingrediente.class).getResultList();
+    }
+}
